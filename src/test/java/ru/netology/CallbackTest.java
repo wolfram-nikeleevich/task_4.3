dd8af9c4fb67f8d0fedcef3f358c8fac98370405
Владимир Сизов <7580844@mail.ru>
2022-09-09 14:12:59 +0300

    test added

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CallbackTest {
@@ -13,13 +16,17 @@ public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    public void setUp() {
+    public static void setUp() {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
     }
 
     @BeforeEach
     void setUpDriver() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -31,12 +38,11 @@ public class CallbackTest {
     @Test
     public void test(){
         driver.get("http://localhost:9999");
-        driver.findElement().sendKeys("Андрей");
-        driver.findElement().sendKeys("+79876543210");
-        driver.findElement().click();
-        driver.findElement().click();
-        driver.findElement().getText();
-        String text = driver.findElement().getText();
-        assertEquals("",text);
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79876543210");
+        driver.findElement(By.className("checkbox__text")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
     }
 }
