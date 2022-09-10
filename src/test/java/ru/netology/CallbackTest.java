4e2fcc088899806ce3db33a4fb21ee3d30aa84d9
Владимир Сизов <7580844@mail.ru>
2022-09-10 13:02:17 +0300

    test modifyed

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,56 +1,21 @@
 package ru.netology;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CallbackTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    public static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-    }
-    
-     @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUpDriver() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void close() {
-        driver.quit();
-        driver = null;
-    }
-
     @Test
-    public void test(){
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
-        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79876543210");
-        driver.findElement(By.className("checkbox__text")).click();
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
+    public void test() {
+        open("http://localhost:9999");
+        $("[type=\"text\"]").setValue("Андрей");
+        $("[type=\"tel\"]").setValue("+79876543210");
+        $(".checkbox__text").click();
+        $(".button__text").click();
+        $(withText("Ваша заявка успешно отправлена!")).should(visible);
     }
 }
